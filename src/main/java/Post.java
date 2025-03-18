166e33d2f19cdbf16804791d689ebfc305f66c65
mrkotyaka <mrkotyaka@gmail.com>
2025-03-18 13:57:42 +0300

    implementation task

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Post {
+
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final String upvotes;
+
+    public Post(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") String upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getUpvotes() {
+        if  (upvotes == null || upvotes.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(upvotes);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "\n id=" + id +
+                "\n text=" + text +
+                "\n type=" + type +
+                "\n user=" + user +
+                "\n upvotes=" + upvotes;
+    }
 }
